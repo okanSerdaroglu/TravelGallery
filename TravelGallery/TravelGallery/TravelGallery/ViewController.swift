d193cd1e4b7f4c8db071551b8a697f53d20ee8e6
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-07-11 13:41:54 +0300

    project initialized

---
+++ TravelGallery/TravelGallery/TravelGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelGallery
+//
+//  Created by Okan Serdaroğlu on 11.07.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
